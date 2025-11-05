23f599b3e455a05566e4015fdcc355068b54288b
emilien samuel <emilien.samuel@etu.univ-lyon1.fr>
2025-11-05 11:57:06 +0100

    focntionnalités 1 : acceuil et statistiques

--- bdw-server/websites/projet/controleurs/accueil.py
+++ bdw-server/websites/projet/controleurs/accueil.py
@@ -1,7 +1,15 @@
-"""
-ce fichier est vide, il faudra y mettre du code (question TP5)
-"""
 from controleurs.includes import add_activity
+from model.model_pg import nombre_equipe, nombre_partie, nombre_morpion ,partie_plus_courte, partie_plus_longue, moyenne_journalisation
+
+#Focntionnalité 1 : accueil et statistiques
+REQUEST_VARS['nb_equipe'] = nombre_equipe(SESSION['CONNEXION'])[0][0] #1 . 3 instances au choix
+REQUEST_VARS['nb_partie'] = nombre_partie(SESSION['CONNEXION'])[0][0] #1
+REQUEST_VARS['nb_morpion'] = nombre_morpion(SESSION['CONNEXION'])[0][0] #1
+REQUEST_VARS['partie_courte'] = partie_plus_courte(SESSION['CONNEXION']) #2 . la durée de la partie la plus courte et de la plus longue
+REQUEST_VARS['partie_longue'] = partie_plus_longue(SESSION['CONNEXION']) #2
+REQUEST_VARS['moyenne_journalisation'] = moyenne_journalisation(SESSION['CONNEXION']) #3 . la moyenne de journalisation par équipe
+
+
 
 add_activity(SESSION['HISTORIQUE'], "consultation de la page d'accueil")
 
--- bdw-server/websites/projet/model/model_pg.py
+++ bdw-server/websites/projet/model/model_pg.py
@@ -46,30 +46,36 @@ def count_instances(connexion, nom_table):
     query = sql.SQL('SELECT COUNT(*) AS nb FROM {table}').format(table=sql.Identifier(nom_table))
     return execute_select_query(connexion, query)
 
+
+"""
+ces fonctions ne seront plus utiles car elles sont relatives à un autre schema de BD
+
 def get_episodes_for_num(connexion, numero):
-    """
+
     Retourne le titre des épisodes numérotés numero
     Integer numero : numéro des épisodes
-    """
+
     query = 'SELECT titre FROM episodes where numéro=%s'
     return execute_select_query(connexion, query, [numero])
 
 def get_serie_by_name(connexion, nom_serie):
-    """
+
     Retourne les informations sur la série nom_serie (utilisé pour vérifier qu'une série existe)
     String nom_serie : nom de la série
-    """
+
     query = 'SELECT * FROM series where nomsérie=%s'
     return execute_select_query(connexion, query, [nom_serie])
 
 def insert_serie(connexion, nom_serie):
-    """
+
     Insère une nouvelle série dans la BD
     String nom_serie : nom de la série
     Retourne le nombre de tuples insérés, ou None
-    """
+
     query = 'INSERT INTO series VALUES(%s)'
     return execute_other_query(connexion, query, [nom_serie])
+"""
+
 
 def get_table_like(connexion, nom_table, like_pattern):
     """
@@ -87,6 +93,104 @@ def get_table_like(connexion, nom_table, like_pattern):
         sql.Placeholder())
     #    like_pattern=sql.Placeholder(name=like_pattern))
     return execute_select_query(connexion, query, [motif])
+
+#Fonctionnalités 1 : accueil et statistiques
+
+#1 . 3 instances au choix
+def nombre_equipe(connexion):
+    """
+    Retourne le nombre d'équipes dans la table équipes
+    """
+    query = 'SELECT COUNT(*) AS nb FROM equipe'
+    return execute_select_query(connexion, query)
+
+def nombre_partie(connexion):
+    """
+    Retourne le nombre de parties dans la table parties
+    """
+    query = 'SELECT COUNT(*) AS nombre_parties FROM partie'
+    return execute_select_query(connexion, query)
+
+def nombre_morpion(connexion):
+    """
+    Retourne le nombre de parties de morpion dans la table morpion
+    """
+    query = 'SELECT COUNT(*) AS nombre_morpion FROM morpion'
+    return execute_select_query(connexion, query)
+
+#2 . la durée de la partie la plus courte et de la plus longue
+def partie_plus_courte(connexion):
+    """
+    Retourne la partie la plus courte dans la table parties
+    """
+    query = '''
+    SELECT
+        ID_Partie,
+        date_debut,
+        date_fin,
+        (date_fin - date_debut) AS duree
+    FROM
+        partie
+    WHERE
+        date_fin IS NOT NULL
+    ORDER BY
+        duree ASC
+    LIMIT 1
+    '''
+    return execute_select_query(connexion, query)
+
+def partie_plus_longue(connexion):
+    """
+    Retourne la partie la plus longue dans la table parties
+    """
+    query = '''
+    SELECT
+        ID_Partie,
+        date_debut,
+        date_fin,
+        (date_fin - date_debut) AS duree
+    FROM
+        partie
+    WHERE
+        date_fin IS NOT NULL
+    ORDER BY
+        duree DESC
+    LIMIT 1
+    '''
+    return execute_select_query(connexion, query)
+
+#3 . la moyenne du nombre de lignes de journalisation par mois et par année
+def moyenne_journalisation(connexion):
+    """
+    Retourne la moyenne du nombre de lignes de journalisation par mois et par année
+    """
+    query= '''
+    SELECT
+        EXTRACT(YEAR FROM P.date_debut) AS annee_partie,
+        EXTRACT(MONTH FROM P.date_debut) AS mois_partie,
+        AVG(T1.nombre_lignes)::numeric(10, 2) AS moyenne_lignes_journalisation
+    FROM
+        partie P
+    JOIN
+        (
+            SELECT
+                ID_Partie,
+                COUNT(id_ligne) AS nombre_lignes
+            FROM
+                ligne_journal
+            GROUP BY
+                ID_Partie
+        ) AS T1 ON P.ID_Partie = T1.ID_Partie
+    GROUP BY
+        annee_partie,
+        mois_partie
+    ORDER BY
+        annee_partie,
+        mois_partie
+    '''
+    return execute_select_query(connexion, query)
+
+
 
 
 
