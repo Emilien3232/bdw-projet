88af87b7b1ad53b3972d2df2c58bfa6c3302c8b5
emilien samuel <emilien.samuel@etu.univ-lyon1.fr>
2025-11-10 23:17:18 +0100

    ajout d'un formulaire dans /ajouter (fonctionnalités 2), pas fini, manque : verification si les données fournies par le fromulaire ne sont pas deja presente dans la bd (couleur et nom) ; fonction ajout_equipe dans le model

--- bdw-server/websites/projet/controleurs/ajouter.py
+++ bdw-server/websites/projet/controleurs/ajouter.py
@@ -1,11 +1,27 @@
-"""
-ce fichier est vide, il faudra y mettre du code (question TP5)
-"""
 from controleurs.includes import add_activity
+from model.model_pg import get_instances 
+
+REQUEST_VARS['morpions'] = get_instances(SESSION['CONNEXION'],'morpion') # on recupere les instances de morpion disponibles pour le formulaire d'ajout d'équipe
+REQUEST_VARS['message_erreur'] = ""
+REQUEST_VARS['message_confirmation'] = ""
+
+#afin de eviter les erreur d'ajout à la base de deonnées 
+#(couleur unique et nom unique) on doit rajouter une comparaison des variables Post avec les elements 
+#deja presents dans la base de données
+
+if POST:
+    try:
+        nom_equipe = POST['nom']
+        couleur_equipe = POST['couleur_equipe']
+        morpion_id = POST['morpions_selectionnes[]'] #recupere les morpions selectionnés par l'utilisateur dans le formulaire
+        
+        if not (6 <= len(morpion_id) <= 8): #si l'utilisateur n'a pas selectionné entre 6 et 8 morpions on renvoie un message d'erreur
+            REQUEST_VARS['message_erreur'] = "Veuillez sélectionner entre 6 et 8 morpions pour l'équipe."
+        else: #si ce n'esst pas le cas on utilise la fonction ajout_equipe et on renvoie un message de confirmation
+            #ajoute_equipe(SESSION['CONNEXION'], nom_equipe, morpion_id, couleur)
+            REQUEST_VARS['message_confirmation'] = "L'équipe a été ajoutée avec succès."
+    except KeyError:
+        REQUEST_VARS['message_erreur'] = "Veuillez séléctionner des morpions."
+
 
 add_activity(SESSION['HISTORIQUE'], "consultation de la page ajouter")
-
-
-
-
-
--- bdw-server/websites/projet/init.py
+++ bdw-server/websites/projet/init.py
@@ -5,8 +5,8 @@ Ficher initialisation (eg, constantes chargées au démarrage dans la session)
 from datetime import datetime
 from os import path
 
-SESSION['APP'] = "Nouveau_titre"
-SESSION['BASELINE'] = "Nouveau_sous-titre"
+SESSION['APP'] = "MORPION 2"
+SESSION['BASELINE'] = "un jeu super"
 SESSION['DIR_HISTORIQUE'] = path.join(SESSION['DIRECTORY'], "historiques")
 SESSION['HISTORIQUE'] = dict()
 SESSION['CURRENT_YEAR'] = datetime.now().year
