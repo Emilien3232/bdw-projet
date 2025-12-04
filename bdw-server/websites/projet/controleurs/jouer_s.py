4fc81967fc76296b7bf3aac717bad1abcf7b1b18
emilien samuel <emilien.samuel@etu.univ-lyon1.fr>
2025-12-04 23:47:02 +0100

    fonctionnalité partie simple presque finis , preciser le retour par POST de morpion_selectionne (retourne un str au lieu d'une liste) , besoin de perfectionner la bascule entre les equipes apres action (voir booleen equipe1_active) , creer un tab 2D ou on reporte la partie de morpions (à utiliser apres ppour un focntions VerifCondVictoires) , creer un RV qui renvoit les coord de la casecibles et l'images a mettre dedans

--- bdw-server/websites/projet/controleurs/jouer_s.py
+++ bdw-server/websites/projet/controleurs/jouer_s.py
@@ -1,20 +1,27 @@
 from controleurs.includes import add_activity
 from model.model_pg import get_morpion_par_equipe
 if SESSION['CONFIG_PARTIE'] :
-    REQUEST_VARS['morpion_equipe_1'] = get_morpion_par_equipe(SESSION['CONNEXION'], SESSION['EQUIPE_1'][0])
+    REQUEST_VARS['morpion_equipe_1'] = get_morpion_par_equipe(SESSION['CONNEXION'], SESSION['EQUIPE_1'][0]) #on recupere les morpions des deux équipes
     REQUEST_VARS['morpion_equipe_2'] = get_morpion_par_equipe(SESSION['CONNEXION'], SESSION['EQUIPE_2'][0])
-    REQUEST_VARS['nb_morpion_equipe_1'] = len(REQUEST_VARS['morpion_equipe_1'])
+    REQUEST_VARS['nb_morpion_equipe_1'] = len(REQUEST_VARS['morpion_equipe_1']) #on calcule le nombre de morpions pour chaque équipe pour la longueur des grilles d'affichage des morpions
     REQUEST_VARS['nb_morpion_equipe_2'] = len(REQUEST_VARS['morpion_equipe_2'])
-    id_equipe_active = None
-    REQUEST_VARS['lancement_partie'] = False
-    if POST :
-        REQUEST_VARS['lancement_partie'] = True
-        if POST['equipe_commence'][0] == '1' :
-            id_equipe_active = SESSION['EQUIPE_1'][0]
-        elif POST['equipe_commence'][0] == '2' :
-            id_equipe_active = SESSION['EQUIPE_2'][0]
+    equipe1_active = True #par défaut l'équipe 1 commence toujours
+
+    REQUEST_VARS['equipe1_active'] = equipe1_active
+        
+    if 'valider_deplacement' in POST :
+        #gestion des erreurs de selection de morpion et case cible
+        if len(POST['morpion_selectionne[]']) > 1 :
+            REQUEST_VARS['message_erreur'] = "Erreur : Vous ne pouvez déplacer qu'un seul morpion à la fois."
+        elif len(POST['case_cible[]']) > 1 :
+            REQUEST_VARS['message_erreur'] = "Erreur : Vous ne pouvez cibler qu'une seule case à la fois."
+        elif len(POST['morpion_selectionne[]']) == 0 :
+            REQUEST_VARS['message_erreur'] = "Erreur : Vous devez sélectionner un morpion à déplacer."
+        elif len(POST['case_cible[]']) == 0 :
+            REQUEST_VARS['message_erreur'] = "Erreur : Vous devez sélectionner une case cible pour le déplacement."
         else :
-            id_equipe_active = SESSION['EQUIPE_1'][0] # par défaut équipe 1 commence, implementer systeme aléatoire plus tard
+            equipe1_active = not(equipe1_active) #on change l'équipe active après un déplacement réussi
+            REQUEST_VARS['equipe1_active'] = equipe1_active 
 
 
 
